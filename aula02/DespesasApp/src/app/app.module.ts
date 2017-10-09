cfc37fbef7ad8399d21da07d9177015e596714fb
essejose <ze_pk2@hotmail.com>
2017-10-09 20:46:39 -0300

    Init project

---
+++ aula02/DespesasApp/src/app/app.module.ts
@@ -0,0 +1,23 @@
+/* Módulos */
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { RoutingModule } from './routing/routing.module';
+/* Componentes */
+import { AppComponent } from './app.component';
+import { ListComponent } from './list/list.component';
+import { AddComponent } from './add/add.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ListComponent,
+    AddComponent
+  ],
+  imports: [
+    BrowserModule,
+    RoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
